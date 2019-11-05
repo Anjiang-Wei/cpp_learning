bafe111b2186c243910863707f8c54cc44d0ea82
Xiuhong Li <lixiuhong@pku.edu.cn>
2019-11-05 16:55:35 +0800

    增加关于循环引用和前置声明的讨论
    Signed-off-by: Xiuhong Li <lixiuhong@pku.edu.cn>

---
+++ header/head1.h
@@ -0,0 +1,10 @@
+#ifndef __HEAD_1_H__
+#define __HEAD_1_H__
+
+#include "head2.h"
+
+#define VAR_MACRO  1          //define a macro, which used in head2.h
+
+bool func(ClassA* CA);        //ClassA is defined in head2.h
+
+#endif 
---
+++ header/head2.h
@@ -0,0 +1,12 @@
+#ifndef __HEAD_2_H__
+#define __HEAD_2_H__
+
+#include "head1.h"
+
+class ClassA{
+  int mVar;
+  void setMem(){ mVar = VAR_MACRO };    //macro VAR_MACRO is defined in head1.h
+
+};
+
+#endif 
